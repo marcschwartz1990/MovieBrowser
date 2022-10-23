82e4b066b3f7566284e45996c602b51ed7be62de
Marc Schwartz <marcschwartz1990@gmail.com>
2022-10-23 12:53:32 -0400

    Initial Commit

---
+++ SamLoBueMockInterview/SamLoBueMockInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamLoBueMockInterviewApp.swift
+//  SamLoBueMockInterview
+//
+//  Created by Marc-Developer on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SamLoBueMockInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
